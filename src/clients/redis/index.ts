40aabc5c7f6e03e132e8e8d5bab39894782de78f
Mohammed-Nur-030 <xyznur786@gmail.com>
2024-03-11 00:13:03 +0530

    Key protection

--- src/clients/redis/index.ts
+++ src/clients/redis/index.ts
@@ -1,5 +1,3 @@
 import Redis from 'ioredis'
 
-export const redisClient = new Redis(
-    "redis://default:67f99ba844d9471d83de8c77c6958dcf@us1-relative-cobra-38513.upstash.io:38513"
-    );
+export const redisClient = new Redis(process.env.REDIS_URL as string);
